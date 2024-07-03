31122fd20e4447d62bfbd19ae11f352d79840b57
SuperChentao <cjin082@uottawa.ca>
2024-07-03 11:49:08 -0400

    1

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,7 @@ import eslintPlugin from '@nabla/vite-plugin-eslint';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react(), eslintPlugin()],
+  base:"/HotelDemo/",
   test: {
     globals: true,
     environment: 'jsdom',
